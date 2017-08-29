ffbf78da9edaa6246a7fb4c253ca816230509b0b
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2017-08-29 12:59:41 -0600

    added node modules to gitignore

--- email.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const nodeMailer = require('nodemailer');
-
-let transporter = nodeMailer.createTransport({
-    host: 'smtp.ex'
-})
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const mailer = require('nodemailer');
+const auth = require('./config.json');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +8,7 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.emailMe = functions.https.onRequest((request, response) => {
+        response.send('Sending you an email', request);
+    });
